cb55c69abeaeaf72091dcf912c4ae18e8f2c7b12
Ruru <maximilien.vanstaevel@epitech.eu>
2025-02-15 18:31:26 +0900

    add links to experience items in ExperiencesSection for improved navigation

--- src/sections/ExperiencesSection.tsx
+++ src/sections/ExperiencesSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-// Replace these imports with the actual paths to your logo files.
 import Insitaction from '../assets/insitation.jpg';
 import EpitechLogo from '../assets/epitech.svg';
 import Riloshost from '../assets/riloshost.svg';
@@ -16,10 +15,7 @@ const ExperiencesSection: React.FC = () => {
             <div className="container mx-auto px-4 py-12">
                 <h2 className="text-4xl font-bold mb-12 text-center">Experiences</h2>
                 <div className="relative">
-                    {/* Vertical line */}
                     <div className="absolute left-1/2 transform -translate-x-1/2 h-full border-l-2 border-gray-600"></div>
-
-                    {/* Experience 1 - Left aligned */}
                     <motion.div
                         className="relative flex items-center mb-12"
                         initial={{ opacity: 0, x: 500 }}
@@ -28,6 +24,7 @@ const ExperiencesSection: React.FC = () => {
                         transition={{ duration: 0.6 }}
                     >
                         <div className="absolute left-1/2 transform -translate-x-1/2 z-10 w-6 h-6 rounded-full bg-blue-500 border-4 border-gray-900"></div>
+                        <a href='https://epitech.eu' target='_blank' rel='noreferrer'>
                         <motion.div
                             initial={{ opacity: 0, x: 500 }}
                             whileInView={{ opacity: 1, x: 150 }}
@@ -46,6 +43,7 @@ const ExperiencesSection: React.FC = () => {
                                 </p>
                             </div>
                         </motion.div>
+                        </a>
                     </motion.div>
 
                     <motion.div
@@ -55,8 +53,8 @@ const ExperiencesSection: React.FC = () => {
                         viewport={{ once: false }}
                         transition={{ duration: 0.6, delay: 0.2 }}
                     >
-                        {/* Dot */}
                         <div className="absolute left-1/2 transform -translate-x-1/2 z-10 w-6 h-6 rounded-full bg-blue-500 border-4 border-gray-900"></div>
+                        <a href='https://epitech.eu' target='_blank' rel='noreferrer'>
                         <motion.div
                             initial={{ opacity: 0, x: -500 }}
                             whileInView={{ opacity: 1, x: -150 }}
@@ -75,9 +73,9 @@ const ExperiencesSection: React.FC = () => {
                                 </p>
                             </div>
                         </motion.div>
+                        </a>
                     </motion.div>
 
-                    {/* Experience 3 - Left aligned */}
                     <motion.div
                         className="relative flex items-center"
                         initial={{ opacity: 0, x: 500 }}
@@ -85,26 +83,28 @@ const ExperiencesSection: React.FC = () => {
                         viewport={{ once: false }}
                         transition={{ duration: 0.6, delay: 0.4 }}
                     >
-                        {/* Dot */}
                         <div className="absolute left-1/2 transform -translate-x-1/2 z-10 w-6 h-6 rounded-full bg-blue-500 border-4 border-gray-900"></div>
-                        <motion.div
-                            initial={{ opacity: 0, x: 500 }}
-                            whileInView={{ opacity: 1, x: 150 }}
-                            viewport={{ once: false }}
-                            transition={{ duration: 0.6, delay: 0.2 }}
-                            className="mr-8 bg-gray-800 p-6 rounded-lg shadow-md flex items-center">
-                            <img
-                                src={Riloshost}
-                                alt="Riloshost Logo"
-                                className="w-12 h-12 mr-4"
-                            />
-                            <div>
-                                <h3 className="text-xl font-bold">Riloshost</h3>
-                                <p className="text-sm text-gray-400">
-                                    2022-now — Internship, Web Development
-                                </p>
-                            </div>
-                        </motion.div>
+                        <a href='https://riloshost.fr' target='_blank' rel='noreferrer'>
+                            <motion.div
+                                initial={{ opacity: 0, x: 500 }}
+                                whileInView={{ opacity: 1, x: 150 }}
+                                viewport={{ once: false }}
+                                transition={{ duration: 0.6, delay: 0.2 }}
+                                className="mr-8 bg-gray-800 p-6 rounded-lg shadow-md flex items-center"
+                            >
+                                <img
+                                    src={Riloshost}
+                                    alt="Riloshost Logo"
+                                    className="w-12 h-12 mr-4"
+                                />
+                                <div>
+                                    <h3 className="text-xl font-bold">Riloshost</h3>
+                                    <p className="text-sm text-gray-400">
+                                        2022-now — Internship, Web Development
+                                    </p>
+                                </div>
+                            </motion.div>
+                        </a>
                     </motion.div>
                 </div>
             </div>
